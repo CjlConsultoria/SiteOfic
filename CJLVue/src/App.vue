1d9fe4e1d02e1ee8027d2744c9f7d294dde883f7
Maria Eduarda Messias <mariaeduardamessias2001@gmail.com>
2025-07-25 13:14:18 -0300

    Atualização

--- CJLVue/src/App.vue
+++ CJLVue/src/App.vue
@@ -172,10 +172,10 @@ div.layout-wrapper(:class="{ 'layout-plataforma': ehPlataforma }")
           RouterLink(to="/sobre") Sobre
           RouterLink(to="/servicos") Serviços
           RouterLink(to="/planos") Planos
-          RouterLink(to="/plataforma") Plataforma
+          //-RouterLink(to="/plataforma") Plataforma
           .mobile-auth-buttons
-            a.external-btn.link-btn(href="/login") Login
-            RouterLink.external-btn.link-btn(to="/registre") Registre-se
+            //-a.external-btn.link-btn(href="/login") Login
+            //-RouterLink.external-btn.link-btn(to="/registre") Registre-se
             button.external-btn(@click="irParaURLExterna") Convivium
 
         //- Menu desktop - visível apenas em telas maiores via CSS
@@ -184,12 +184,12 @@ div.layout-wrapper(:class="{ 'layout-plataforma': ehPlataforma }")
           RouterLink(to="/sobre") Sobre
           RouterLink(to="/servicos") Serviços
           RouterLink(to="/planos") Planos
-          RouterLink(to="/plataforma") Plataforma
+          //-RouterLink(to="/plataforma") Plataforma
 
         //- Botões de login/registro
         .auth-buttons
-          a.external-btn.link-btn(href="/login") Login
-          RouterLink.external-btn.link-btn(to="/registre") Registre-se
+          //-a.external-btn.link-btn(href="/login") Login
+          //-RouterLink.external-btn.link-btn(to="/registre") Registre-se
           button.external-btn(@click="irParaURLExterna") Convivium
 
   //- Conteúdo principal
